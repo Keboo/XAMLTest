f82882754609c5520ab9872b0678e2ed5bcb9f94
Kevin Bost <kitokeboo@gmail.com>
2020-10-03 22:37:13 -0700

    Allow saving screenshot with provided suffix.
    Added unit tests for SaveScreenshot

---
+++ XAMLTest.Tests/Simulators/App.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace XamlTest.Tests.Simulators
+{
+
+    public class App : IApp
+    {
+        public Task<IWindow> CreateWindow(string xaml)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IWindow> CreateWindow<TWindow>() where TWindow : Window
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IWindow?> GetMainWindow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResource> GetResource(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IImage> GetScreenshot()
+            => Task.FromResult<IImage>(new Image());
+
+        public Task<IReadOnlyList<IWindow>> GetWindows()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Initialize(string applicationResourceXaml, params string[] assemblies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XAMLTest.Tests/Simulators/Image.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace XamlTest.Tests.Simulators
+{
+    public class Image : IImage
+    {
+        public Task Save(Stream stream) => Task.CompletedTask;
+    }
+}
