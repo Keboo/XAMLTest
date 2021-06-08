3ad4ea93ddbeca29493f94355fb5ada8c8e887fb
Kevin Bost <kitokeboo@gmail.com>
2021-06-08 00:42:02 -0700

    Starting on grid stuff

---
+++ XAMLTest/Transport/GridSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace XamlTest.Transport
+{
+    internal class GridSerializer : ISerializer
+    {
+        public bool CanSerialize(Type type)
+            => type == typeof(ColumnDefinitionCollection) ||
+               type == typeof(RowDefinitionCollection) ||
+               type == typeof(ColumnDefinition) ||
+               type == typeof(RowDefinition);
+
+        public object? Deserialize(Type type, string value) => throw new NotImplementedException();
+        public string Serialize(Type type, object? value)
+        {
+
+        }
+    }
+}
