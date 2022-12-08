cc073e2c74fc6ac5288245c713d7b44904730daa
Kevin Bost <kitokeboo@gmail.com>
2022-12-08 00:34:00 -0800

    Removing some dead code

--- XAMLTest/Program.cs
+++ XAMLTest/Program.cs
@@ -163,7 +163,6 @@ internal class Program
         AppDomain.CurrentDomain.IncludeAssembliesIn(Path.GetDirectoryName(assemblyPath)!);
 
         var targetAssembly = Assembly.LoadFile(assemblyPath);
-        AppDomain.CurrentDomain.SetData("APP_CONTEXT_BASE_DIRECTORY", Path.GetDirectoryName(assemblyPath));
         Application application;
         if (remoteMethodName != null &&
             remoteContainerType != null &&
@@ -177,7 +176,6 @@ internal class Program
                 ?? throw new XamlTestException($"Did not find factory method {remoteMethodName} in {remoteContainerType}");
             application = (Application)(factoryMethod.Invoke(null, Array.Empty<object>())
                 ?? throw new XamlTestException("Factory method did return an application instance"));
-            Application.ResourceAssembly = factoryAssembly;
         }
         else
         {
